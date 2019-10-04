3c5e3054335a630f72a8165d7d26429bac5e8e07
Rafa <wirecobweb@gmail.com>
2019-10-04 12:11:41 +0200

    first commit

---
+++ src/index.ts
@@ -0,0 +1,12 @@
+import Swal from 'sweetalert2'
+//import './assets/styles/styles.css';
+
+//import './assets/styles/style.scss';
+
+
+function alarma(){
+    Swal.fire('Hello world!')
+}
+
+
+alarma()
---
+++ src/services/indexeddbService.ts
@@ -0,0 +1,6 @@
+import Dexie from 'dexie';
+import IProduct from '../models/product.ts'
+
+class MyAppDatabase extends Dexie {
+  
+}
